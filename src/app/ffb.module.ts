cb01167c52683860c7ad29294aaecb89e13afa7d
Matthew Green <greeny88@gmail.com>
2020-02-05 14:34:35 -0600

    More cosmetic changes

--- src/app/ffb.module.ts
+++ src/app/ffb.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,6 +12,7 @@ import './ffb.scss';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        FlexLayoutModule,
         FormsModule,
         ComponentModule
     ],
