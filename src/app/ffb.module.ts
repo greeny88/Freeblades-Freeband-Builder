f0215e133ac2514431141ea97acc9376195a5d22
Matthew Green <greeny88@gmail.com>
2021-10-22 22:00:18 -0500

    Fixed the display of the nav items.

--- src/app/ffb.module.ts
+++ src/app/ffb.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -23,6 +24,7 @@ import './ffb.scss';
         ComponentModule,
         MatButtonModule,
         MatIconModule,
+        MatListModule,
         MatSidenavModule,
         MatSnackBarModule,
         MatToolbarModule
